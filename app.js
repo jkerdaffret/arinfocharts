e6bada87391df244f2c057ea2b54ffc460aa0871
jkerdaffret <54045225+jkerdaffret@users.noreply.github.com>
2020-03-30 12:09:19 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,388 @@
+var lrsydev = document.getElementById('lrsydev').getContext('2d');
+var chart = new Chart(lrsydev, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ['Réussite Totale', 'Réussite Partielle'],
+        datasets: [{
+            label: "Réussite à l'examen",
+            backgroundColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)'
+        ],
+            borderColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)'
+        ],
+            data: [86, 14]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+var lrsydes = document.getElementById('lrsydes').getContext('2d');
+var chart = new Chart(lrsydes, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ['Réussite Totale', 'Réussite Partielle'],
+        datasets: [{
+            label: "Réussite à l'examen",
+            backgroundColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)'
+        ],
+            borderColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)'
+        ],
+            data: [75, 25]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+var lrsyi = document.getElementById('lrsyi').getContext('2d');
+var chart = new Chart(lrsyi, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ['Réussite Totale', 'Réussite Partielle'],
+        datasets: [{
+            label: "Réussite à l'examen",
+            backgroundColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)'
+        ],
+            borderColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)'
+        ],
+            data: [91, 9]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+var lrsyb = document.getElementById('lrsyb').getContext('2d');
+var chart = new Chart(lrsyb, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ['Réussite Totale', 'Réussite Partielle'],
+        datasets: [{
+            label: "Réussite à l'examen",
+            backgroundColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)'
+        ],
+            borderColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)'
+        ],
+            data: [83, 17]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+var ndev = document.getElementById('ndev').getContext('2d');
+var chart = new Chart(ndev, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ['Réussite Totale', 'Echec'],
+        datasets: [{
+            label: "Réussite à l'examen",
+            backgroundColor: [
+                'rgb(99, 132, 255)',
+                'rgb(132, 132, 132)'
+        ],
+            borderColor: [
+                'rgb(99, 132, 255)',
+                'rgb(132, 132, 132)'
+        ],
+            data: [50, 50]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+var ndes = document.getElementById('ndes').getContext('2d');
+var chart = new Chart(ndes, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ['Réussite Totale', 'Réussite Partielle'],
+        datasets: [{
+            label: "Réussite à l'examen",
+            backgroundColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)'
+        ],
+            borderColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)'
+        ],
+            data: [72, 28]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+var ni = document.getElementById('ni').getContext('2d');
+var chart = new Chart(ni, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ['Réussite Totale', 'Réussite Partielle'],
+        datasets: [{
+            label: "Réussite à l'examen",
+            backgroundColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)'
+        ],
+            borderColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)'
+        ],
+            data: [77, 23]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+var nb = document.getElementById('nb').getContext('2d');
+var chart = new Chart(nb, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ['Réussite Totale', 'Réussite Partielle'],
+        datasets: [{
+            label: "Réussite à l'examen",
+            backgroundColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)'
+        ],
+            borderColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)'
+        ],
+            data: [75, 25]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+var lmdes = document.getElementById('lmdes').getContext('2d');
+var chart = new Chart(lmdes, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ['Réussite Totale', 'Réussite Partielle'],
+        datasets: [{
+            label: "Réussite à l'examen",
+            backgroundColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)'
+        ],
+            borderColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)'
+        ],
+            data: [78, 22]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+var lmi = document.getElementById('lmi').getContext('2d');
+var chart = new Chart(lmi, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ['Réussite Totale', 'Réussite Partielle'],
+        datasets: [{
+            label: "Réussite à l'examen",
+            backgroundColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)'
+        ],
+            borderColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)'
+        ],
+            data: [79, 21]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+var lmb = document.getElementById('lmb').getContext('2d');
+var chart = new Chart(lmb, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ['Réussite Totale'],
+        datasets: [{
+            label: "Réussite à l'examen",
+            backgroundColor: [
+                'rgb(99, 132, 255)'
+        ],
+            borderColor: [
+                'rgb(99, 132, 255)'
+        ],
+            data: [100]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+var pdes = document.getElementById('pdes').getContext('2d');
+var chart = new Chart(pdes, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ['Réussite Totale', 'Réussite Partielle', 'Echec'],
+        datasets: [{
+            label: "Réussite à l'examen",
+            backgroundColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)',
+                'rgb(132,132,132)'
+        ],
+            borderColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)',
+                'rgb(132,132,132)'
+        ],
+            data: [7, 33, 60]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+var pb = document.getElementById('pb').getContext('2d');
+var chart = new Chart(pb, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ['Réussite Totale', 'Réussite Partielle'],
+        datasets: [{
+            label: "Réussite à l'examen",
+            backgroundColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)'
+        ],
+            borderColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)'
+        ],
+            data: [78, 22]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+var stni = document.getElementById('stni').getContext('2d');
+var chart = new Chart(stni, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ['Réussite Totale'],
+        datasets: [{
+            label: "Réussite à l'examen",
+            backgroundColor: [
+                'rgb(99, 132, 255)'
+        ],
+            borderColor: [
+                'rgb(99, 132, 255)'
+        ],
+            data: [100]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+var stnb = document.getElementById('stnb').getContext('2d');
+var chart = new Chart(stnb, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ['Réussite Totale', 'Réussite Partielle'],
+        datasets: [{
+            label: "Réussite à l'examen",
+            backgroundColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)'
+        ],
+            borderColor: [
+                'rgb(99, 132, 255)',
+                'rgb(200, 132, 29)'
+        ],
+            data: [75, 25]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
